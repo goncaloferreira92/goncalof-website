8d84389753399d7a2664cf75ee7c4cc7583ab7f5
Gonçalo Ferreira <goncaloferreira@ua.pt>
2021-01-29 11:27:27 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState } from 'react';
+import { setTokenSourceMapRange } from 'typescript';
+import './css/App.css';
+
+const App = (): JSX.Element => {
+
+  const [name, setName] = useState('Goncalo');
+
+  return (
+    <React.Fragment>
+      <div>
+        <div style={{ position: 'fixed', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+          <button>
+            <a href='#home'>
+              Home
+            </a>
+          </button>
+          <button>
+            <a href='#projects'>
+              Projects
+            </a>
+          </button>
+          <button>
+            <a href='#about'>
+              About
+            </a>
+          </button>
+          <button onClick={() => setName(name + ' clicked')}> Change name </button>
+          <button onClick={() => setName('Goncalo')}> Reset name </button>
+          <div>{name}</div>
+        </div>
+        <div style={{ backgroundColor: 'orange', width: '100vw', height: '100vh' }} id='home' />
+        <div style={{ backgroundColor: 'green', width: '100vw', height: '100vh' }} id='projects' />
+        <div style={{ backgroundColor: 'blue', width: '100vw', height: '100vh' }} id='about' />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
