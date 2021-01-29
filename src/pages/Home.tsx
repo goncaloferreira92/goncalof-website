0620644ead1b7904edc5200bf2a1effe7b8f215f
Gonçalo Ferreira <goncaloferreira@ua.pt>
2021-01-29 12:16:02 +0100

    Home.tsx into a cmp

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{ backgroundColor: 'orange', width: '100vw', height: '100vh' }} id='home'>
+        </div>
+    )
+}
+
+export default Home;
