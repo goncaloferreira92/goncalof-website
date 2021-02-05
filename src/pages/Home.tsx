275ebed98fa1fe144e1889b61284749c9899096e
Gonçalo Ferreira <goncalo.ferreira@datathings.com>
2021-02-05 22:07:28 +0100

    Home Page image transition issue solved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,20 @@
+
 import GoncaloPhoto from '../img/goncalo_photo.png';
 
 const Home = () => {
+    const transitionOnMouseLeave = () => {
+        let element: any = document.querySelector('.goncalo-photo');
+        element != null && (element.style.transition = '200ms ease-in-out');
+        // TODO: there should be a way of knowing when the animation stops and perform a callback e.g.: requestAnimationFrame();
+        setTimeout(() => {
+            element != null && (element.style.transition = 'none');
+            // This is to set the transition only to the hover, otherwise the Auto Dimension will also be slaggy. :)
+            // https://stackoverflow.com/questions/26095149/why-is-document-queryselector-not-working-on-pseudo-element
+            var sheet = document.styleSheets[0];
+            var rules = sheet.rules;
+            sheet.insertRule('.goncalo-photo:hover { transition: 200ms ease-in-out; }', rules.length);
+        }, 300);
+    }
     return (
         <div className='home-main-container' id='home'>
 
@@ -13,7 +27,7 @@ const Home = () => {
             </div>
 
             <div className='goncalo-photo-container'>                
-                <img src={GoncaloPhoto} className='goncalo-photo' alt='goncalo-main' />
+                <img src={GoncaloPhoto} className='goncalo-photo' alt='goncalo-main' onMouseLeave={() => transitionOnMouseLeave()} />
             </div>
             <div className='text-and-links'>
                 <h1>
